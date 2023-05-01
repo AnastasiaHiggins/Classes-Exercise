69f3083edcf4f4139f8c987a2c4d9b1fb187fb0b
Anastasia higgins <higgins.ana@gmail.com>
2023-05-01 12:34:40 -0400

    first commit

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+    public Car() { }
+
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    }
+
+    //    Create a class named Car - make sure it is public
+    //    In the Car class:
+    //        Create a Make property of type string that is public
+    //        Create a Model property of type string that is public
+    //        Create a Year property of type int that is public
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //    In the Main method:
+            //Create a new instance of the Car class
+            //Set values in the properties for the object
+            //Print the values of each property to the Console
+
+            Car blueCar = new Car()
+            {
+                Make = "Mustang",
+                Model = "GT-500",
+                Year = 2021
+            };
+
+
+            Console.WriteLine(blueCar.Make);
+            Console.WriteLine(blueCar.Model);
+            Console.WriteLine(blueCar.Year);
+
         }
-    }
+}
 }
